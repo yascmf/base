86b531135834e0172b1161533b5caae647a172fe
raoyc <raoyc2009@gmail.com>
2016-07-14 13:34:32 +0800

    update language detect

--- app/Jobs/SetLocale.php
+++ app/Jobs/SetLocale.php
@@ -5,6 +5,7 @@ namespace App\Jobs;
 use App\Jobs\Job;
 use Request;
 use Illuminate\Contracts\Bus\SelfHandling;
+use Douyasi\Locator\PreferredLanguageDetector;
 
 class SetLocale extends Job implements SelfHandling
 {
@@ -18,8 +19,14 @@ class SetLocale extends Job implements SelfHandling
         $site = Request::input('site', 'desktop');
         $site_langs = config('site.lang.'.$site);
         $sid = 'yascmf.lang.'.$site;
+        /*
         if (!session()->has($sid)) {
             session()->put($sid, Request::getPreferredLanguage($site_langs));
+        }
+        */
+        $preferral_language = with(new PreferredLanguageDetector)->detect($site_langs);
+        if (!session()->has($sid)) {
+            session()->put($sid, $preferral_language);
         }
 
         app()->setLocale(session($sid));
